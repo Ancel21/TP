e076eae248258adfd49a7ce4b2132cb939b83421
ANCEL YOUNG NKAMNGANG TCHANA <ancelyoung@yahoo.fr>
2023-01-31 23:44:46 +0100

    TP3 Springboot new version

--- src/main/java/com/ancel/TP3/controller/AddressController1.java
+++ src/main/java/com/ancel/TP3/controller/AddressController1.java
@@ -1,5 +1,23 @@
 package controller;
 
-public class AddressController1 {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.Projet.Tp3.model.Address;
+import com.example.Projet.Tp3.model.AddressRepository;
+
+
+import org.springframework.ui.Model;
+
+
+@controller
+public class AddressController1 {
+    @Autowired
+    AddressRepository addressRepository;
+    @GetMapping("/addresse")
+    public String showAdresse(Model model){
+        model.addAttribute("addresse",new Address());
+        return "addresse";
+    }
 }
